b37c9c8e6e560e995d20c2beb4a05603ec8b09cd
Aleksandar Vuckovic <tamper1974@hotmail.rs>
2022-02-13 13:20:25 +0100

    Update matrix-main.py

--- matrix-main.py
+++ matrix-main.py
@@ -49,7 +49,16 @@ def mymatrix():
       for j in range(3):
          pmatx[i][j] = matx1[j][0] * matx2[0][j]+matx1[j][1] * matx2[1][j]+matx1[j][2] * matx2[2][j]
     print(pmatx)
-    
+
+class MyHTMLParser(HTMLParser):
+    def handle_starttag(self, tag, attrs):
+        print("Encountered a start tag:", tag)
+
+    def handle_endtag(self, tag):
+        print("Encountered an end tag :", tag)
+
+    def handle_data(self, data):
+        print("Encountered some data  :", data)
     
 parser = MyHTMLParser()
 parser.feed('<html><head><title>Test</title></head>'
