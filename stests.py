a234b9378911f3389b6049335606c702e8efcfb1
Aleksandar Vuckovic <tamper1974@hotmail.rs>
2022-02-13 14:18:55 +0100

    Update stests.py

--- stests.py
+++ stests.py
@@ -1,42 +1,37 @@
 import streamlit as st
 import streamlit.components.v1 as components
 
-# bootstrap 4 collapse example
 components.html(
     """
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
-    <div id="accordion">
-      <div class="card">
-        <div class="card-header" id="headingOne">
-          <h5 class="mb-0">
-            <button class="btn btn-link" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
-            Collapsible Group Item #1
-            </button>
-          </h5>
-        </div>
-        <div id="collapseOne" class="collapse show" aria-labelledby="headingOne" data-parent="#accordion">
-          <div class="card-body">
-            Collapsible Group Item #1 content
-          </div>
-        </div>
-      </div>
-      <div class="card">
-        <div class="card-header" id="headingTwo">
-          <h5 class="mb-0">
-            <button class="btn btn-link collapsed" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
-            Collapsible Group Item #2
-            </button>
-          </h5>
-        </div>
-        <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordion">
-          <div class="card-body">
-            Collapsible Group Item #2 content
-          </div>
-        </div>
-      </div>
-    </div>
+    
+    <head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>ADRESAR LOKACIJA</title>
+	<style type="text/css">
+	 body{margin: 0px;background-image:url('back.jpg');}
+	 header{padding: 10px;text-align: center;background-color:#ffffaa;
+	 }	
+	 div{background-color: #000000;color: #ffffff;width:30%;height:40%;margin:auto;padding:50px;margin-top:5%;}
+     
+	</style>
+</head>
+<body>
+<header>ADRESAR LOKACIJA</header>
+<main>
+ <div><input type=text><label>POSTANSKI BROJ</label><input type="radio" value="true"><label>RAMPA</label><input type="radio" value="false">
+ </div>
+ <div>
+  <a>RAMPA</a><a>35</a><br>	
+  <a>MESTO</a><a>Ralja</a><br>	
+  <a>GRAD/OPSTINA</a><a>Smederevo</a><br>	
+  <a>OKRUG</a><a>Podunavski okrug</a><br>	
+ </div>
+ <table>
+  <tr><td>11050</td><td>33</td><td>Medakovi</td><td>11050</td>
+ </table>
+</main>
+</body>
     """,
     height=600,
 )
